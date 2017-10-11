068cb157b17d1c044fb651bbd8d45fb055b2c7a3
mfgrayse <mjcgray@comcast.net>
2017-10-11 11:33:57 -0700

    Added URLs for CSS examples and GitHub repo

--- Recipe Organizer MVC/Global.asax.cs
+++ Recipe Organizer MVC/Global.asax.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//GitHub repo info: https://github.com/mfgrayse/Recipe-Organizer-MVC
+//CSS examples: https://www.bettycrocker.com/recipes/ultimate-chocolate-chip-cookies/77c14e03-d8b0-4844-846d-f19304f61c57
+// and http://www.delish.com/cooking/recipe-ideas/recipes/a49067/chicken-noodle-soup-casserole-recipe/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
